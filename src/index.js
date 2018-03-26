738e40458fd4f9a04bd6c62e5d9de3b78581d1ff
Dragos Nedelcu <dragos.nedelcu@lokalleads.de>
2018-03-26 12:50:37 +0200

    adding provider

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { injectIntl, IntlProvider, FormattedRelative } from "react-intl";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const PostDate = injectIntl(({ date, intl }) => (
+  <span title={intlFormatDate(date)}>
+    <FormattedRelative value={date} />
+  </span>
+));
+
+const App = ({ post: { date, title, body } }) => (
+  <div>
+    <h1>{title}</h1>
+    <p>PostDate date={date}</p>
+    <div>{body}</div>
+  </div>
+);
+
+ReactDOM.render(
+  <IntlProvider locale={navigator.language}>
+    <App
+      post={{
+        title: "Hello, World!",
+        date: new Date(1459913574887),
+        body: "Amazing content"
+      }}
+    />
+  </IntlProvider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
