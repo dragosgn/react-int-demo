c95a14664c1258e9bd9159fe88522b14f81e5f32
Dragos Nedelcu <dragos.nedelcu@lokalleads.de>
2018-04-05 11:56:27 +0200

    fixed

--- src/messages.js
+++ src/messages.js
@@ -1,11 +1,5 @@
+import english from "./english.json";
+
 export default {
-  de: {
-    "body.greeting": "Hallo, wie geht es bei dir?"
-  },
-  en: {
-    "body.greeting": "Hey man, whats up?"
-  },
-  es: {
-    "body.greeting": "Hola amigo, como te va?"
-  }
+  en: english
 };
