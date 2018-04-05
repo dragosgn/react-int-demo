d8e1d0fca3cfbaaf384263428ea9e5c7a19ab840
Dragos Nedelcu <dragos.nedelcu@lokalleads.de>
2018-04-05 14:47:12 +0200

    cleansing

--- src/messages.js
+++ src/messages.js
@@ -1,9 +1,11 @@
 import en from "./translations/en.json";
 import de from "./translations/de.json";
 import es from "./translations/es.json";
+import fr from "./translations/fr.json";
 
 export default {
   en: en,
   de: de,
-  es: es
+  es: es,
+  fr: fr
 };
